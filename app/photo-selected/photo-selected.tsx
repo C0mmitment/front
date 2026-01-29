5f713b8ad323fa1111f7d7adf0fc6747954a6248
kinoshita <171928933+yukikinoshita09@users.noreply.github.com>
2026-01-29 14:57:43 +0900

    [change] ボタンの見た目変更

--- app/photo-selected/photo-selected.tsx
+++ app/photo-selected/photo-selected.tsx
@@ -1,9 +1,9 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Image, Text, View } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
-import { Button } from "../components/Button/Button";
+import { Image, Text, View, Pressable } from "react-native";
+import { useLocalSearchParams } from "expo-router";
 import * as Sharing from "expo-sharing";
 import * as FileSystem from "expo-file-system/legacy";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function PhotoSelectedPage() {
   const { uri } = useLocalSearchParams<{ uri: string }>();
@@ -38,9 +38,28 @@ export default function PhotoSelectedPage() {
         )}
       </View>
 
-      <View className="flex flex-row p-5 gap-3 justify-center">
-        <Button text="AIに聞く" onPress={() => console.log("click")} />
-        <Button text="共有する" onPress={shareImage} />
+      <View className="flex flex-row p-5 gap-10 justify-center">
+        {/* AIに聞くボタン */}
+        <Pressable
+          className="flex-col items-center gap-3"
+          onPress={() => {console.log("AIに聞く")}}
+        >
+          <View className="w-16 h-16 items-center justify-center border-2 border-primary rounded-full">
+            <FontAwesome6 name="robot" size={28} color="#FF73EF" />
+          </View>
+          <Text className="text-secondary">AIに聞く</Text>
+        </Pressable>
+
+        {/* 共有するボタン */}
+        <Pressable
+          className="flex-col items-center gap-3"
+          onPress={shareImage}
+        >
+          <View className="w-16 h-16 items-center justify-center border-2 border-primary rounded-full">
+            <FontAwesome6 name="share-nodes" size={28} color="#FF73EF" />
+          </View>
+          <Text className="text-secondary">共有する</Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
