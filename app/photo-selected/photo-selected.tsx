f2dc0699f99ee00e5dbf706f7e7e0835993ff19a
leon <2240206ecc@gmail.com>
2026-01-30 16:40:01 +0900

    [refactor] アドバイスページと写真選択ページのアイコンボタンの色を変更し、矢印画像のパスを修正

--- app/photo-selected/photo-selected.tsx
+++ app/photo-selected/photo-selected.tsx
@@ -1,6 +1,6 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Image, Text, View, Pressable } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { Image, Text, View, TouchableOpacity } from "react-native";
+import { useLocalSearchParams, router } from "expo-router";
 import * as Sharing from "expo-sharing";
 import * as FileSystem from "expo-file-system/legacy";
 import { useEffect, useState } from "react";
@@ -9,6 +9,11 @@ import type { VisualCue } from "../api/advice-api";
 import PhotoTips from "../components/Tips/tips";
 import IconButton from "../components/icon-button/icon-button";
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { colors } from "../constans/color";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import ArrowImg from "../../assets/arrow.png"
+import Animated from "react-native-reanimated";
+import { usePhotoAdviceVisuals } from "../hooks/usePhotoAdviceVisuals";
 
 export default function PhotoSelectedPage() {
   const [advice, setAdvice] = useState<string>();
@@ -16,6 +21,35 @@ export default function PhotoSelectedPage() {
   const [visualCue, setVisualCue] = useState<VisualCue | null>(null);
   const { uri } = useLocalSearchParams<{ uri: string }>();
   const [ratio, setRatio] = useState<number>(1);
+  const [box, setBox] = useState<{ w: number; h: number } | null>(null);
+
+  const showVisuals = !isLoading && !!visualCue;
+
+  const { arrowStyle, arrowPositionStyle, boxStyle, isDepth } =
+    usePhotoAdviceVisuals({
+      direction: visualCue?.direction,
+    });
+
+  // 画像をbox内で最大サイズにするための計算
+  const fitted = (() => {
+    if (!box || !ratio) return null;
+    const W = box.w;
+    const H = box.h;
+
+    // まず幅を最大にして高さを計算
+    let w = W;
+    let h = W / ratio;
+
+    // 高さが超えるなら高さを最大に幅を計算
+    if (h > H) {
+      h = H;
+      w = H * ratio;
+    }
+
+    return { w, h };
+  })();
+
+
 
   useEffect(() => {
     if (!uri) return;
@@ -43,6 +77,7 @@ export default function PhotoSelectedPage() {
     }
   }
 
+  // アドバイス取得
   async function fetchAdvice() {
     if (!uri || isLoading) return;
 
@@ -65,34 +100,71 @@ export default function PhotoSelectedPage() {
   return (
     <SafeAreaView className="flex-1 bg-white">
 
-      <View className="flex-1 items-center justify-center">
+      {/* 上部 UI */}
+      <View className="bg-white px-4 py-5 flex-row items-center justify-start">
+        <TouchableOpacity onPress={() => router.back()}>
+          <Ionicons name="arrow-back" size={28} color={colors.secondary} />
+        </TouchableOpacity>
+      </View>
+
+      {/* 写真表示エリア */}
+      <View
+        className="flex-1 items-center justify-center bg-gray-100"
+        onLayout={(e) => {
+          const { width, height } = e.nativeEvent.layout;
+          setBox({ w: width, h: height });
+        }}
+      >
         {uri ? (
-          <View className="w-full overflow-hidden " style={{ aspectRatio: ratio }}>
-            <Image source={{ uri }} className="w-full h-full" resizeMode="contain" />
-          </View>
+          fitted ? (
+            <View style={{ width: fitted.w, height: fitted.h }} className="relative">
+              {/* 画像 */}
+              <Image source={{ uri }} className="w-full h-full" resizeMode="contain" />
+
+              {/* 枠 */}
+              {showVisuals && isDepth && (
+                <Animated.View
+                  className="absolute top-[20%] left-[15%] w-[70%] h-[60%] border-4 border-yellow-300 rounded-xl"
+                  style={boxStyle}
+                />
+              )}
+
+              {/* 矢印 */}
+              {showVisuals && !isDepth && (
+                <Animated.Image
+                  source={ArrowImg}
+                  className="absolute w-20 h-20 tint-blue-300"
+                  style={[arrowPositionStyle, arrowStyle]}
+                  resizeMode="contain"
+                />
+              )}
+            </View>
+
+          ) : null
         ) : (
           <Text className="text-black">画像がありません</Text>
         )}
       </View>
+
       {/* Tips & アドバイス */}
       <View className="m-4">
-          {isLoading ? (
-            <PhotoTips />
-          ) : (
-            <Text className="text-red-500">{advice}</Text>
-          )}
-        </View>
+        {isLoading ? (
+          <PhotoTips />
+        ) : (
+          <Text className="text-red-500">{advice}</Text>
+        )}
+      </View>
       <View className="flex flex-row p-5 gap-10 justify-center">
         {/* AIに聞くボタン */}
         <IconButton
-          icon={<FontAwesome6 name="robot" size={28} color="#FF73EF" />}
+          icon={<FontAwesome6 name="robot" size={28} color={colors.primary} />}
           label="AIに聞く"
           onPress={fetchAdvice}
         />
 
         {/* 共有するボタン */}
         <IconButton
-          icon={<FontAwesome6 name="share-nodes" size={28} color="#FF73EF" />}
+          icon={<FontAwesome6 name="share-nodes" size={28} color={colors.primary} />}
           label="共有する"
           onPress={shareImage}
         />
